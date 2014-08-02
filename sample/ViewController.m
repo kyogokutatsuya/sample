565316c71458865e41113ab62eaee647a4f47f70
kyogokutatsuya <kyogokutatsuya@gmail.com>
2014-08-02 20:02:28 +0900

    hoge2を出力

--- sample/ViewController.m
+++ sample/ViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     NSLog(@"hoge");
-    NSLog(@"hoge");
+    NSLog(@"hoge2");
 }
 
 - (void)didReceiveMemoryWarning
