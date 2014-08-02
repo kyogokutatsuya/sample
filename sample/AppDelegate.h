fdd5fb8f79c58898352a1cd9e9489d09028edb08
kyogokutatsuya <kyogokutatsuya@gmail.com>
2014-08-02 19:21:32 +0900

    Initial Commit

---
+++ sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sample
+//
+//  Created by kyogokutatsuya on 2014/08/02.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ sample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  sample
+//
+//  Created by kyogokutatsuya on 2014/08/02.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
